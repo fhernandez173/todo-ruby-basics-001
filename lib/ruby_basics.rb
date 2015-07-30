fb1cf8407fc272886b618b3ebc2edad7cc5c8aff
fhernandez173 <f.hernandez173@gmail.com>
2015-07-30 13:20:35 -0400

    done

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,36 @@
 def division(num1, num2)
- 
+  num1 / num2
+end
+division(12,2)
+
+def assign_variable(value = "Bob")
+  return value
+end
+assign_variable("")
+
+def argue(args)
+  return args
+end
+argue(1)
+
+def greeting(arg1, arg2)
+
 end
 
-def assign_variable(value)
-  
+def return_a_value(rValue = "Nice")
+  return rValue
 end
-
-def argue
-  
-end
-
-def greeting
-
-end
-
-def return_a_value
-
-end
+return_a_value("")
 
 def last_evaluated_value
-
+  return "expert"
 end
+last_evaluated_value
 
-def pizza_party
-
+def pizza_party(option = "cheese")
+  return option
 end
+pizza_party("")
+
+
+
